7f21a5ee43f314c27a42fb88740afdd89f748eb2
Just Lest <just.lest@gmail.com>
2009-06-09 11:50:45 +0300

    empty plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_time_limit do
+  name 'Redmine Time Limit plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
